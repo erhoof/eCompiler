5aa640559d53f046bec92da030d93b284af93305
erhoof <erhoof@icloud.com>
2020-06-20 11:05:07 +0300

    Lexer: implemented Num Token

---
+++ include/Lexer/Num.h
@@ -0,0 +1,20 @@
+//
+// Created by Pavel Bibichenko on 20.06.2020.
+//
+
+#ifndef ECOMPILER_NUM_H
+#define ECOMPILER_NUM_H
+
+#include "Token.h"
+
+class Num : public Token {
+public:
+    explicit Num(int value);
+
+    std::string toString() const;
+
+private:
+    int m_value;
+};
+
+#endif //ECOMPILER_NUM_H
---
+++ src/Lexer/Num.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Pavel Bibichenko on 20.06.2020.
+//
+
+#include "../../include/Lexer/Num.h"
+
+#include "../../include/Lexer/Tag.h"
+
+Num::Num(int value) : Token(Tag::NUM) {
+    m_value = value;
+}
+
+std::string Num::toString() const {
+    return std::to_string(m_value);
+}
