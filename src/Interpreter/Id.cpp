718851766e28e7318ae861fe7862b3b0ca50861e
erhoof <erhoof@icloud.com>
2020-06-20 15:45:26 +0300

    Interpreter: adds object - Id

---
+++ src/Interpreter/Id.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Pavel Bibichenko on 20.06.2020.
+//
+
+#include "../../include/Interpreter/Id.h"
+
+Id::Id(Word& id, Type& type, int offset) : Expression(id, type) {
+    m_offset = offset;
+}
+
+int Id::offset() const {
+    return m_offset;
+}
