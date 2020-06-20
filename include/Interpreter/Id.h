6e7fd4d54b87531e7f7f3bed4c360ef1ccfdd98f
erhoof <erhoof@icloud.com>
2020-06-20 15:45:34 +0300

    Interpreter: adds object - Id

---
+++ include/Interpreter/Id.h
@@ -0,0 +1,20 @@
+//
+// Created by Pavel Bibichenko on 20.06.2020.
+//
+
+#ifndef ECOMPILER_ID_H
+#define ECOMPILER_ID_H
+
+#include "Expression.h"
+
+class Id : public Expression {
+public:
+    Id(Word& id, Type& type, int offset);
+
+    int offset() const;
+
+private:
+    int m_offset;
+};
+
+#endif //ECOMPILER_ID_H
