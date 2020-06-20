1d35ccad3d1d6af93215410bc376e78ac57f1f66
erhoof <erhoof@icloud.com>
2020-06-20 10:52:43 +0300

    Lexer: implemented Tags

---
+++ include/Lexer/Tag.h
@@ -0,0 +1,33 @@
+//
+// Created by Pavel Bibichenko on 20.06.2020.
+//
+
+#ifndef ECOMPILER_TAG_H
+#define ECOMPILER_TAG_H
+
+class Tag {
+public:
+    static int
+        AND,
+        ELSE,
+        ID,
+        MINUS,
+        REAL,
+        BASIC,
+        EQ,
+        IF,
+        NE,
+        TEMP,
+        BREAK,
+        FALSE,
+        INDEX,
+        NUM,
+        TRUE,
+        DO,
+        GE,
+        LE,
+        OR,
+        WHILE;
+};
+
+#endif //ECOMPILER_TAG_H
---
+++ src/Lexer/Tag.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Pavel Bibichenko on 20.06.2020.
+//
+
+#include "../../include/Lexer/Tag.h"
+
+int Tag::AND    = 256;
+int Tag::ELSE   = 260;
+int Tag::ID     = 264;
+int Tag::MINUS  = 268;
+int Tag::REAL   = 272;
+int Tag::BASIC  = 257;
+int Tag::EQ     = 261;
+int Tag::IF     = 265;
+int Tag::NE     = 269;
+int Tag::TEMP   = 273;
+int Tag::BREAK  = 258;
+int Tag::FALSE  = 262;
+int Tag::INDEX  = 266;
+int Tag::NUM    = 270;
+int Tag::TRUE   = 274;
+int Tag::DO     = 259;
+int Tag::GE     = 263;
+int Tag::LE     = 267;
+int Tag::OR     = 271;
+int Tag::WHILE  = 275;
