b1267d472d5794697a545bf7b11db897836c86fb
erhoof <erhoof@icloud.com>
2020-06-20 14:16:19 +0300

    Symbol Table: implemented Type Table

---
+++ include/SymbolTable/TypeTable.h
@@ -0,0 +1,26 @@
+//
+// Created by Pavel Bibichenko on 20.06.2020.
+//
+
+#ifndef ECOMPILER_TYPETABLE_H
+#define ECOMPILER_TYPETABLE_H
+
+#include "Type.h"
+
+class TypeTable {
+public:
+    static TypeTable& instance();
+
+    TypeTable(const TypeTable&) = delete;
+    TypeTable& operator=(const TypeTable&) = delete;
+
+    // Type Table
+    Type* t_int;
+    Type* t_float;
+    Type* t_char;
+    Type* t_bool;
+private:
+    TypeTable();
+};
+
+#endif //ECOMPILER_TYPETABLE_H
---
+++ src/SymbolTable/TypeTable.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Pavel Bibichenko on 20.06.2020.
+//
+
+#include "../../include/SymbolTable/TypeTable.h"
+#include "../../include/Lexer/Tag.h"
+
+TypeTable &TypeTable::instance() {
+    static TypeTable TypeTable;
+    return TypeTable;
+}
+
+TypeTable::TypeTable() {
+    t_int = new Type("int",     Tag::BASIC, 4);
+    t_float = new Type("float", Tag::BASIC, 8);
+    t_char = new Type("char",   Tag::BASIC, 1);
+    t_bool = new Type("bool",   Tag::BASIC, 1);
+}
