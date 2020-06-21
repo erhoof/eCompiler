702bf3a3dcd86f55ea2a7671c54fd50e0d0a4d1e
erhoof <erhoof@icloud.com>
2020-06-21 15:12:04 +0300

    Interpreter: new object - Not

---
+++ include/Interpreter/Not.h
@@ -0,0 +1,19 @@
+//
+// Created by Pavel Bibichenko on 21.06.2020.
+//
+
+#ifndef ECOMPILER_NOT_H
+#define ECOMPILER_NOT_H
+
+#include "Logical.h"
+
+class Not : public Logical {
+public:
+    Not(Token& token, Expression* x2);
+
+    void jumping(int t, int f);
+
+    std::string toString() const;
+};
+
+#endif //ECOMPILER_NOT_H
---
+++ src/Interpreter/Not.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Pavel Bibichenko on 21.06.2020.
+//
+
+#include "../../include/Interpreter/Not.h"
+
+Not::Not(Token& token, Expression* x2) : Logical(token, x2, x2) {}
+
+void Not::jumping(int t, int f) {
+    expr2()->jumping(f, t);
+}
+
+std::string Not::toString() const {
+    std::string out =  operand().toString() + " " + expr2()->toString();
+
+    return out;
+}
